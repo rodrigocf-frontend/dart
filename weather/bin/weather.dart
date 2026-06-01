3c7e357f143f4afeaf0453f9a42e8476c9c5553b
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2026-06-01 10:16:51 -0300

    Weather CLI (Initial Commit)

---
+++ weather/bin/weather.dart
@@ -0,0 +1,5 @@
+import 'package:weather/weather.dart' as weather;
+
+void main(List<String> arguments) {
+  print('Hello world: ${weather.calculate()}!');
+}
---
+++ weather/test/weather_test.dart
@@ -0,0 +1,8 @@
+import 'package:weather/weather.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
